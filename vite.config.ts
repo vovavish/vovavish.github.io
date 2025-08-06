cc02b28d5e580dae44f76ecac030faf564f012b8
vovavish <vovavishniakov856@gmail.com>
2025-08-06 19:07:08 +0300

    feat: add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vovavish.github.io/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
